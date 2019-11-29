8de30e78afba7db886d2d6e175dad1a7bad5e535
Samarth Agarwal <samarthagarwal@live.com>
2019-11-29 21:18:38 +0530

    Controllers Done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from "@angular/router";
+import { LoadingController, ToastController, AlertController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -46,7 +47,7 @@ export class HomePage {
 
   designations: any [] = ["Developer", "Manager", "Teacher"];
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private loadingCtrl: LoadingController, private toastCtrl: ToastController, private alertCtrl: AlertController, private actionSheetCtrl: ActionSheetController) {
     
   }
 
@@ -54,16 +55,96 @@ export class HomePage {
   //   document.querySelector('#mySelect').shadowRoot.querySelector('.select-text').setAttribute('style', 'white-space:normal');
   // }
 
-  ionViewDidEnter() {
-    console.log("ionViewDidEnter Home Page");
+  async showLoading() {
+
+    let loading = await this.loadingCtrl.create({
+      spinner: "dots",
+      message: "Loading, please wait..."
+    });
+
+    loading.present();
+
+    setTimeout(() => {
+      loading.dismiss();
+    }, 5000)
+
   }
 
-  navigate() {
-    this.router.navigate(["/office"]);
+  async showToast() {
+
+    let toast = await this.toastCtrl.create({
+      message: "Creating new post...",
+      showCloseButton: true,
+      closeButtonText: "OK",
+      position: "top"
+    });
+
+    toast.present();
+
   }
 
-  onClicked(user: any) {
-    console.log(user);
+  async showAlert() {
+    let alert = await this.alertCtrl.create({
+      header: "Delete",
+      subHeader: "Are you sure?",
+      message: "This item will be deleted forever!",
+      inputs: [{
+        name: "developer",
+        type: "checkbox",
+        label: "Developer",
+        value: "developer"
+      }, {
+        name: "jrdeveloper",
+        type: "checkbox",
+        label: "Jr. Developer",
+        value: "jrdeveloper"
+      }, {
+        name: "manager",
+        type: "checkbox",
+        label: "Manager",
+        value: "manager"
+      }],
+      buttons: [{
+        text: "Cancel",
+        handler: () => {
+          console.log("User tapped on Cancel");
+        }
+      }, {
+        text: "OK",
+        handler: (data) => {
+
+          console.log(data);
+
+        }
+      }]
+    });
+
+    alert.present();
+  }
+
+  async showActionSheet() {
+    let actionSheet = await this.actionSheetCtrl.create({
+      header: "Select Payment Mode",
+      subHeader: "Please select any one of the following payment methods",
+      buttons: [{
+        text: "Credit Card",
+        handler: () => {
+
+        }
+      }, {
+        text: "Debit Card",
+        handler: () => {
+          
+        }
+      }, {
+        text: "Bank transfer",
+        handler: () => {
+          
+        }
+      }]
+    })
+
+    actionSheet.present();
   }
 
 }
